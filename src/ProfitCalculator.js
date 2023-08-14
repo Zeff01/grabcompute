051a6226226611240c9c22cfdc657883608cdbf9
Zeff01 <jzeffsomera@gmail.com>
2023-08-15 00:19:37 +0800

    optimize

--- src/ProfitCalculator.js
+++ src/ProfitCalculator.js
@@ -17,14 +17,18 @@ const ProfitCalculator = () => {
     return value.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   };
   const netProfit = parsedBoundary * 26 - parsedMonthlyAmortization;
-  const ROI = parsedCashout / netProfit;
+  const ROI = (parsedCashout / netProfit).toFixed(2);
+  const handleInputChange = (value, setter) => {
+    // Remove commas, then check if the input is a valid number with up to 2 decimal places
+    if (/^\d*\.?\d{0,2}$/.test(value.replace(/,/g, ""))) {
+      setter(formatNumber(value.replace(/,/g, "")));
+    }
+  };
 
   return (
     <>
       <div className=" h-auto  bg-[#E4E4E4] con p-10  relative  flex flex-col justify-center items-center px-5 lg:h-screen ">
         <img src={Car} className="z-10 w-[700px]" />
-        {/* <img src={black} className="absolute -top-[50px] left-0 z-1" />
-        <img src={black} className="absolute  " /> */}
         <div className="flex flex-col bg p-8  container  gap-4 rounded-xl lg:flex-row lg:items-center lg:justify-center lg:gap-10">
           <p className="text-xl font-bold mb-2 text-white text-center lg:text-2xl">
             GRAB COMPUTATION ROI
@@ -40,9 +44,7 @@ const ProfitCalculator = () => {
               type="text"
               id="cashout"
               value={cashout}
-              onChange={(e) =>
-                setCashout(formatNumber(e.target.value.replace(/,/g, "")))
-              }
+              onChange={(e) => handleInputChange(e.target.value, setCashout)}
               className="mt-1 p-2 w-full text-white rounded-md bg-[#676767] outline-none"
             />
           </div>
@@ -52,15 +54,13 @@ const ProfitCalculator = () => {
               htmlFor="boundary"
               className="block text-md font-medium text-white "
             >
-              Boundary
+              Driver's Boundary per day
             </label>
             <input
               type="text"
               id="boundary"
               value={boundary}
-              onChange={(e) =>
-                setBoundary(formatNumber(e.target.value.replace(/,/g, "")))
-              }
+              onChange={(e) => handleInputChange(e.target.value, setBoundary)}
               className="mt-1 p-2 w-full  rounded-md bg-[#676767] text-white outline-none"
             />
           </div>
@@ -77,22 +77,19 @@ const ProfitCalculator = () => {
               id="monthlyAmortization"
               value={monthlyAmortization}
               onChange={(e) =>
-                setMonthlyAmortization(
-                  formatNumber(e.target.value.replace(/,/g, ""))
-                )
+                handleInputChange(e.target.value, setMonthlyAmortization)
               }
               className="mt-1 p-2 w-full  rounded-md bg-[#676767] text-white outline-none"
             />
           </div>
 
           <div className="flex flex-col gap-2">
-            <p className="text-xl font-bold text-white underline ">
-              Net Profit: {netProfit}
+            <p className="text-md font-medium text-white  ">
+              Monthly Net Profit: {netProfit}
             </p>
-            <div className="flex gap-2 text-xl lg:text-xl font-bold text-white">
-              <p className=" underline">
-                ROI: {isFinite(ROI) ? ROI : "N/A"} Months
-              </p>
+
+            <div className="flex gap-2 text-md lg:text-xl font-medium text-white ">
+              <p className=" ">ROI: {isFinite(ROI) ? ROI : "N/A"} Months</p>
             </div>
           </div>
         </div>
